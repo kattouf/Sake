9129e194391aceaa2d4e9f76102f49402ebdb535
Kattouf <vasiliy.kattouf@gmail.com>
2024-09-17 12:24:31 +0700

    update SakeApp deps

--- SakeApp/Package.swift
+++ SakeApp/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         
     ],
     dependencies: [
-        .package(url: "https://github.com/kattouf/Sake", branch: "main"),
+        .package(url: "https://github.com/kattouf/Sake", from: "0.1.0"),
         .package(url: "https://github.com/kareman/SwiftShell", from: "5.1.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
