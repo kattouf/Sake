e336dd92fc9d612259b9fba6de9ff7369da68dae
Kattouf <vasiliy.kattouf@gmail.com>
2024-09-22 15:30:32 +0700

    add tests commands; update build artifacts command

--- SakeApp/Sakefile.swift
+++ SakeApp/Sakefile.swift
@@ -1,3 +1,4 @@
+import CryptoKit
 import Foundation
 import Sake
 import SwiftShell
@@ -7,7 +8,9 @@ import SwiftShell
 struct Commands: SakeApp {
     public static var buildReleaseArtifacts: Command {
         Command(
+            description: "Build release artifacts",
             run: { _ in
+                var archivePaths = [String]()
                 let triples = ["x86_64-apple-macosx", "arm64-apple-macosx"]
                 let executableName = "sake"
                 for triple in triples {
@@ -19,7 +22,37 @@ struct Commands: SakeApp {
 
                     let executableArchivePath = ".build/artifacts/\(executableName)-\(triple).zip"
                     try runAndPrint("zip", "-j", executableArchivePath, executablePath)
+                    archivePaths.append(executableArchivePath)
                 }
+
+                var shasumResults = [String]()
+                for archivePath in archivePaths {
+                    let file = FileHandle(forReadingAtPath: archivePath)!
+                    let shasum = SHA256.hash(data: file.readDataToEndOfFile())
+                    let shasumString = shasum.compactMap { String(format: "%02x", $0) }.joined()
+                    shasumResults.append("\(shasumString)  \(archivePath)")
+                }
+                FileManager.default.createFile(atPath: ".build/artifacts/shasum", contents: shasumResults.joined(separator: "\n").data(using: .utf8))
+
+                print("Release artifacts built successfully at '.build/artifacts/'")
+            }
+        )
+    }
+
+    public static var unitTests: Command {
+        Command(
+            description: "Run unit tests",
+            run: { _ in
+                try runAndPrint("swift", "test", "--filter", "^(?!.*\\bIntegrationTests\\b).*", "--parallel")
+            }
+        )
+    }
+
+    public static var integrationTests: Command {
+        Command(
+            description: "Run integration tests",
+            run: { _ in
+                try runAndPrint("swift", "test", "--filter", "IntegrationTests", "--parallel")
             }
         )
     }
