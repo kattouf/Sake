2202b38df779f17064d1cf007db2510c71c5f901
Kattouf <vasiliy.kattouf@gmail.com>
2024-09-22 13:35:29 +0700

    build executable for test

--- Tests/IntegrationTests/IntegrationTests.swift
+++ Tests/IntegrationTests/IntegrationTests.swift
@@ -3,15 +3,21 @@ import XCTest
 
 final class IntegrationTests: XCTestCase {
     func testHelloWorld() throws {
+        let buildResult = SwiftShell.run(bash: "swift build -c release --product sake")
+        if !buildResult.succeeded {
+            XCTFail("Failed to build Sake: stdout: \(buildResult.stdout), stderr: \(buildResult.stderror)")
+        }
+        let sakePath = FileManager.default.currentDirectoryPath + "/.build/release/sake"
+
         let tempDirectory = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
         let sakeAppPath = tempDirectory.appendingPathComponent("my-sake-app").path
 
-        let sakeInitResult = SwiftShell.run(bash: "sake init --sake-app-path \(sakeAppPath)")
+        let sakeInitResult = SwiftShell.run(bash: "\(sakePath) init --sake-app-path \(sakeAppPath)")
         if !sakeInitResult.succeeded {
             XCTFail("Failed to init Sake app: stdout: \(sakeInitResult.stdout), stderr: \(sakeInitResult.stderror)")
         }
 
-        let sakeRunResult = SwiftShell.run(bash: "sake hello --sake-app-path \(sakeAppPath)")
+        let sakeRunResult = SwiftShell.run(bash: "\(sakePath) hello --sake-app-path \(sakeAppPath)")
         if sakeRunResult.succeeded {
             XCTAssertTrue(sakeRunResult.stdout.contains("Hello, world!"))
         } else {
