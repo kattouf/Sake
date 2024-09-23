9edbf3b8235697e495454427a44259e5fb28e93b
Kattouf <vasiliy.kattouf@gmail.com>
2024-09-23 11:38:31 +0700

    release flow commands

--- SakeApp/Sakefile.swift
+++ SakeApp/Sakefile.swift
@@ -1,3 +1,4 @@
+import ArgumentParser
 import CryptoKit
 import Foundation
 import Sake
@@ -55,9 +56,37 @@ struct TestCommands {
 
 @CommandGroup
 struct ReleaseCommands {
-    public static var buildReleaseArtifacts: Command {
+    private static let buildArtifactsDirectory = ".build/artifacts"
+
+    struct ReleaseArguments: ParsableArguments {
+        @Argument(help: "Version number")
+        var version: String
+
+        func validate() throws {
+            guard version.range(of: #"^\d+\.\d+\.\d+$"#, options: .regularExpression) != nil else {
+                throw ValidationError("Invalid version number. Should be in the format 'x.y.z'")
+            }
+        }
+    }
+
+    public static var release: Command {
+        Command(
+            description: "Release",
+            dependencies: [buildReleaseArtifacts],
+            run: { context in
+                let arguments = try ReleaseArguments.parse(context.arguments)
+                try arguments.validate()
+
+                try runAndPrint("sake", "createAndPushTag", arguments.version)
+                try runAndPrint("sake", "draftReleaseWithArtifacts", arguments.version)
+            }
+        )
+    }
+
+    private static var buildReleaseArtifacts: Command {
         Command(
             description: "Build release artifacts",
+            dependencies: [cleanReleaseArtifacts],
             run: { _ in
                 var archivePaths = [String]()
                 let triples = ["x86_64-apple-macosx", "arm64-apple-macosx"]
@@ -69,7 +98,7 @@ struct ReleaseCommands {
                     let executablePath = run("swift", "build", buildFlags, "--show-bin-path").stdout + "/\(executableName)"
                     try runAndPrint("strip", "-rSTx", executablePath)
 
-                    let executableArchivePath = ".build/artifacts/\(executableName)-\(triple).zip"
+                    let executableArchivePath = "\(buildArtifactsDirectory)/\(executableName)-\(triple).zip"
                     try runAndPrint("zip", "-j", executableArchivePath, executablePath)
                     archivePaths.append(executableArchivePath)
                 }
@@ -86,7 +115,47 @@ struct ReleaseCommands {
                     contents: shasumResults.joined(separator: "\n").data(using: .utf8)
                 )
 
-                print("Release artifacts built successfully at '.build/artifacts/'")
+                print("Release artifacts built successfully at '\(buildArtifactsDirectory)'")
+            }
+        )
+    }
+
+    private static var cleanReleaseArtifacts: Command {
+        Command(
+            description: "Clean release artifacts",
+            run: { _ in
+                try runAndPrint("rm", "-rf", buildArtifactsDirectory)
+            }
+        )
+    }
+
+    private static var createAndPushTag: Command {
+        Command(
+            description: "Create and push a tag",
+            run: { context in
+                let arguments = try ReleaseArguments.parse(context.arguments)
+                try arguments.validate()
+
+                let version = arguments.version
+
+                try runAndPrint("git", "tag", version)
+                try runAndPrint("git", "push", "origin", "tag", version)
+            }
+        )
+    }
+
+    private static var draftReleaseWithArtifacts: Command {
+        Command(
+            description: "Draft a release on GitHub",
+            run: { context in
+                let arguments = try ReleaseArguments.parse(context.arguments)
+                try arguments.validate()
+
+                let tagName = arguments.version
+                let releaseTitle = arguments.version
+                let draftReleaseCommand =
+                    "gh release create \(tagName) \(buildArtifactsDirectory)/* --title '\(releaseTitle)' --draft --verify-tag --generate-notes"
+                try runAndPrint(draftReleaseCommand)
             }
         )
     }
